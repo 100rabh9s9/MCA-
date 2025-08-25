f886395ba40ab63324ead0e6fe4d358f31526041
100rabh9s9 <100rabh9s9@gmail.com>
2025-08-25 21:19:56 +0530

    Add Java Folder with Class Method and Objects files

---
+++ Java/Class Method and Objects/Bank.java
@@ -0,0 +1,43 @@
+// Write down a java program to define a class bank with instance vairables accno,name,acctype and balamt. 
+// Define a method to sotre  the values into the variables and another method to display the same.
+// Define an object and call the method
+import java.util.Scanner;
+
+//import org.omg.CORBA.TypeCodePackage.BadKind;
+class bank
+{
+    int accno;
+    String name,acctype;
+    float balamt;
+
+    public static void main(String[] args)
+     {
+         bank b = new bank();
+         b.inputdetail();
+         b.printdetails();
+        
+    }
+    public void inputdetail()
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Account Number:");
+        accno = sc.nextInt();
+        System.out.println("Enter Name : ");
+        name = sc.next();
+        System.out.println("Enter Account Type:");
+        acctype = sc.next();
+        System.out.println("Enter Balance:");
+        balamt = sc.nextFloat();
+        sc.close();
+    }
+    public void printdetails()
+    {
+        System.out.println("\n");
+        System.out.println("Details: ");
+        System.out.println("\n");
+        System.out.println("Account Number:"+this.accno);
+        System.out.println("Name:"+this.name);
+        System.out.println("Account Type:"+this.acctype);
+        System.out.println("Balance in Account :"+this.balamt);
+    }
+}
---
+++ Java/Class Method and Objects/Studentsdemo.java
@@ -0,0 +1,19 @@
+class Students
+{
+  int studno;
+  String studname;
+}
+class Studentsdemo
+{
+   public static void main(String args[])
+    {
+       Students s1 = new Students();
+	s1.studno=101;
+	s1.studname="Rajesh";
+	System.out.println(s1.studno);
+	System.out.println(s1.studname);
+	//Students s2 = new Students();
+    }
+}
+	
+	
