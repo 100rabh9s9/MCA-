7aaab0c94cafba952cd45e2cb1b16fadda070e22
SOURABH S <100rabh9s9@gmail.com>
2025-09-05 21:21:32 +0530

    Character Array using Pointers

--- Data Structures/PointerPractice/Pointer3.c
+++ Data Structures/PointerPractice/Pointer3.c
@@ -7,9 +7,9 @@ int main()
     char *ptr;
     printf("\n Enter Size of Array : ");
     scanf("%d",&size);
-    getchar();
+    getchar();  // To consume the newline character left in the input buffer after scanf
     
-    ptr = (char*)malloc((size+1)*sizeof(char));
+    ptr = (char*)malloc((size+1)*sizeof(char));  // +1 is for the null terminator '\0' to make it a valid C string
     
     if(ptr == NULL)
     {
@@ -18,7 +18,7 @@ int main()
     printf("\n Enter %d Characters:",size);
     for(int i = 0 ; i < size ; i ++ )
     {
-        scanf(" %c",ptr+i);
+        scanf(" %c",ptr+i); // The space before %c ensures it skips any whitespace characters
         //*(ptr + i) = getchar();
     }
     
@@ -33,7 +33,7 @@ int main()
     
     printf("\nAs a string: %s\n", ptr);
     
-    free(ptr);
+    free(ptr);  // Free the dynamically allocated memory
     return 0;
   
 }
