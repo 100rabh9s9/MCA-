d1f66245fe3a1ee9c01089cf3c5c891fc9dc80b8
SOURABH S <100rabh9s9@gmail.com>
2025-10-06 23:28:22 +0530

    JaggedArray.java

--- Java/Class Method and Objects/JaggedArray.java
+++ Java/Class Method and Objects/JaggedArray.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
-
-public class JaggedArray {
+public class JaggedArray 
+{
     public static void main(String[] args) 
     {
         Scanner scanner = new Scanner(System.in);
@@ -11,17 +11,14 @@ public class JaggedArray {
 
         // jagged array
         int[][] jaggedArray = new int[rows][];
-
-        // Populate the jagged array
         for (int i = 0; i < rows; i++) 
         {
             System.out.print("Enter the number of elements in row " + (i + 1) + ": ");
             int columns = scanner.nextInt();
-
+            
             // Create the sub-array with the user-defined number of columns
             jaggedArray[i] = new int[columns];
 
-        
             System.out.println("Enter " + columns + " elements for row " + (i + 1) + ":");
             for (int j = 0; j < columns; j++) 
             {
@@ -39,8 +36,6 @@ public class JaggedArray {
             }
             System.out.println();
         }
-
-        // Close the scanner
         scanner.close();
     }
 }
